3b08b616616e99e8187430febe6930aa06861d41
AryanNag29 <231030332@juitsolan.in>
2025-05-20 22:31:48 +0530

    padding of collision

--- Prop.cpp
+++ Prop.cpp
@@ -1,25 +1,27 @@
 #include "raylib.h"
 #include "raymath.h"
 #include "Prop.h"
-
+//prop constructor
 Prop::Prop(Vector2 pos, Texture2D tex) : worldPos(pos),
                                          texture(tex)
 {
 }
-
+//draw prop
 void Prop::Render(Vector2 knightPos)
 {
     Vector2 screenPos = Vector2Subtract(worldPos, knightPos);
     DrawTextureEx(texture, screenPos, 0.f, scale, WHITE);
 }
-
+//collision of prop
 Rectangle Prop ::GetCollisionRec(Vector2 knightPos)
 {
     Vector2 screenPos = Vector2Subtract(worldPos, knightPos);
-    float pad{10.f};
+    //to make prop rec small
+    float padW{20.f};
+    float padH{40.f};
     return Rectangle{
         screenPos.x,
         screenPos.y,
-        texture.width * scale -pad,
-        texture.height * scale -pad};
+        texture.width * scale -padW,
+        texture.height * scale -padH};
 }
